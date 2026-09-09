39b7048636d7b3c4feae3b6bd622e0ee2fe8ef87
co2xsolution <co2xsolution@gmail.com>
2026-09-09 21:37:06 +0800

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 // The only file you edit when you redeploy the Apps Script backend.
 window.LIMS_CONFIG = {
-  apiUrl: 'https://script.google.com/macros/s/AKfycby4Vvzhd1laQwom2zuma8IMrQG--kg72r-hulbaD815g80weIIRQhCFegRVPGeUh-NB/exec',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbzm47Y7NLJO8J6D0N7PXelMjW_pRV3JU2tmk63taz9xJ3LCQ8Wy4rYt5oFlBTWXkE3g/exec',
   labName: 'Laboratory'   // placeholder until bootstrap returns the real name
 };
